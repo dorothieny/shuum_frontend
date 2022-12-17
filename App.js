ca00913277e56a85ed686ffb175923574374519b
Ponomareva Daria <dorothinein@gmail.com>
2022-12-17 23:15:01 +0300

    small update to check
    Now I am a mobile app developer, трепещите

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, ActivityIndicator, RefreshControl, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components/native';
+
+const MainContainer = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    `;
 
 export default function App() {
+  const [state, setState] = useState();
+  const [isLoading, setIsLoading]=useState(true);
+
+ const fetchSoundCards = () => {
+  setIsLoading(true)
+  axios.get('http://localhost:3000/api/v1/soundcards/').then((r) => {
+    setState(r.data.data)
+  }).finally(() => {
+    setIsLoading(false)
+  })
+ }
+
+  useEffect(() => {
+    fetchSoundCards()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <MainContainer>
+      {/* <ActivityIndicator size="large"/> */}
+            <FlatList 
+            refreshControl={<RefreshControl refreshing={isLoading} onRefresh={fetchSoundCards}/>}
+            style={{backgroundColor: 'lightgray'}} 
+            data={state} 
+            renderItem={({item, i}) => {
+              return (
+                <TouchableOpacity>
+                    <Text>{item.name}</Text>
+                </TouchableOpacity>
+              )
+            }} />
       <StatusBar style="auto" />
-    </View>
+    </MainContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
