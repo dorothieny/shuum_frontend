01c1a4bf2b726578bd308063404729eeda3d7235
Ponomareva Daria <dorothinein@gmail.com>
2022-12-21 04:01:39 +0300

    wow it sounds!
    but only m4a(((

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View, ActivityIndicator, RefreshControl, TouchableOpacity } from 'react-native';
+import { FlatList, StyleSheet, Text, View, ActivityIndicator, RefreshControl, TouchableOpacity, Image, Button } from 'react-native';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
+import { Audio } from 'expo-av';
+
+const StyledImage = styled.Image`
+  height: 100px;
+  width: 100px;
+`
 
 const HomeScreen = ({ navigation }) => {
     const [state, setState] = useState();
     const [isLoading, setIsLoading]=useState(true);
+    const [sound, setSound] = useState();
 
    const fetchSoundCards = () => {
     axios.get('http://localhost:3000/api/v1/soundcards/').then((r) => {
@@ -16,6 +23,17 @@ const HomeScreen = ({ navigation }) => {
     })
    }
   
+  const playSound = async(link) => {
+    // alert.m](link);
+    // alert(`http://localhost:3000${link}`)
+    const sound = new Audio.Sound()
+    await sound.loadAsync({
+      uri: `http://localhost:3000${link}`
+  })
+  
+  await sound.playAsync()
+  }
+
    const sliceName = (name) => {
     if(name.length >= 50) {
       return name.substring(0, 50) + "..."
@@ -27,8 +45,14 @@ const HomeScreen = ({ navigation }) => {
       fetchSoundCards()
     }, [])
 
-
-
+    useEffect(() => {
+      return sound
+        ? () => {
+            console.log('Unloading Sound');
+            sound.unloadAsync();
+          }
+        : undefined;
+    }, [sound]);
     return (
        <>
        {isLoading ? (
@@ -42,10 +66,16 @@ const HomeScreen = ({ navigation }) => {
             data={state} 
             renderItem={({item, i}) => {
               return (
-                <TouchableOpacity onPress={() => navigation.navigate("Player", {id: item.id})}>
-                    <Text>{sliceName(item.name)}</Text>
-                    <Text>{new Date(item.created_at).toLocaleDateString()}</Text>
-                </TouchableOpacity>
+                <View>
+                    <TouchableOpacity onPress={() => navigation.navigate("Player", {id: item.id})}>
+                      <StyledImage 
+                      source={{uri: `http://localhost:3000${item?.image?.url}`}}
+                      />
+                        <Text>{sliceName(item.name)}</Text>
+                        <Text>{new Date(item.created_at).toLocaleDateString()}</Text>
+                    </TouchableOpacity>
+                  <Button title='Play' onPress={() => playSound(item?.audiofile?.url)} />
+                </View>
               )
             }} />
          )}
